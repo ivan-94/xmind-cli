0b0b2a1ed9999adb1523f9cfc1b1507b84bff7c9
ivan <carney520@hotmail.com>
2026-05-22 09:23:38 +0800

    Support query path contains

--- src/domain/query.rs
+++ src/domain/query.rs
@@ -62,6 +62,9 @@ impl QueryComparison {
             (QueryField::Path, QueryOperator::Eq, QueryValue::String(expected)) => {
                 path.to_selector_value() == *expected
             }
+            (QueryField::Path, QueryOperator::Contains, QueryValue::String(needle)) => {
+                path.to_selector_value().contains(needle)
+            }
             (QueryField::Path, QueryOperator::StartsWith, QueryValue::String(prefix)) => {
                 path.to_selector_value().starts_with(prefix)
             }
